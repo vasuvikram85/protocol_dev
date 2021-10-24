895ef9fc88a31dad1a3cd5f313760934b04894d5
vasu <vasuvikram85@gmail.com>
2021-10-25 01:31:58 +0530

    ProgramUpdated

--- intro/gettinginput.c
+++ intro/gettinginput.c
@@ -1,18 +1,12 @@
 #include<stdio.h>
 int main()
-{
-	
-	
-	int a,b,c,d,e,f,g;
-	scanf("%d%d%d",&a,&b,&c);
+{	
+		
+	float a,b,c,d,e,f,g;
+	scanf("%f%f%f",&a,&b,&c);
 	d=a+b+c;e=a-b-c;f=a*b*c;g=a/b/c;
-	printf("a value is %d\n",a);
-	printf("b value is %d\n",b);
-	printf("c value is %d\n",c);
-        printf("d value is %d\n",d);
-	printf("e value is %d\n",e);
-	printf("f value is %d\n",f);
-	printf("g value is %d\n",g);
+	printf("%f\n%f\n%f\n%f\n",d,e,f,g);
+	/*printf("%d\n%d\n%d\n",a,b,c);*/
 	return 0;
 }
 
